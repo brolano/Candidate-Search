f4c627d0d3a9df02c84f5431547eaff122e362bc
Brian Solano <brian_solano110012@yahoo.com>
2025-01-05 23:42:30 -0600

    updated candidate search ts

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,12 @@
+import { NavLink } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav className='nav'>
+        <div><NavLink to="/" className='nav-link'>Home</NavLink></div>
+        <div><NavLink to="/SavedCandidates" className='nav-link'>Potential Candidates</NavLink></div>
+    </nav>
   )
 };
 
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,40 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
-interface GithubUser {
+export interface Candidate {
     login: string;
     id: number;
-    node_id: string;
     avatar_url: string;
-    gravatar_id: string;
     url: string;
     html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    stawrred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-}
-
-interface GithubUserDetail extends GithubUserDetail {
-    name: string | null;
-    company: string | null;
-    blog: string | null;
-    location: string | null;
-    email: string | null;
-    hireable: string | null;
-    bio: string | null;
-    twitter_username: string | null;
-    public_repos: number;
-    public_gists: number;
-    followers: number;
-    folowing: number;
-    created_at: string;
-    updated_at: string;
-
-
+    bio: string;
+    company: string;
+    email: string;
+    location: string;
 }
